8b2007a38709f3267f0efb36110fbcf5c4c227d8
who-hoo <who.ho3ov@gmail.com>
2022-10-01 01:52:24 +0900

    ♻️ [2강] safe call과 elvis 연산자를 이용하여 코틀린스러운 코드로 개선

--- src/lecture02/kotlin/Main.kt
+++ src/lecture02/kotlin/Main.kt
@@ -3,24 +3,16 @@ package lecture02.kotlin
 import java.lang.IllegalArgumentException
 
 fun startsWithA1(str: String?): Boolean {
-    if (str == null) {
-        throw IllegalArgumentException("null이 들어왔습니다.")
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A")
+        ?: throw IllegalArgumentException("null이 들어왔습니다.")
 }
 
 fun startsWithA2(str: String?): Boolean? {
-    if (str == null) {
-        return null
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A")
 }
 
 fun startsWithA3(str: String?): Boolean {
-    if (str == null) {
-        return false
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A") ?: false
 }
 
 fun main() {
