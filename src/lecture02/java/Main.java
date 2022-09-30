b25a15d8a703446cc07d8365f0d48f7b17939ead
who-hoo <who.ho3ov@gmail.com>
2022-10-01 01:48:21 +0900

    ✨ [2강] 코틀린에서의 null 체크

---
+++ src/lecture02/java/Main.java
@@ -0,0 +1,29 @@
+package lecture02.java;
+
+public class Main {
+
+    public boolean startsWithA1(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException("null이 들어왔습니다.");
+        }
+        return str.startsWith("A");
+    }
+
+    public Boolean startsWithA2(String str) {
+        if (str == null) {
+            return null;
+        }
+        return str.startsWith("A");
+    }
+
+    public boolean startsWithA3(String str) {
+        if (str == null) {
+            return false;
+        }
+        return str.startsWith("A");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/lecture02/kotlin/Main.kt
@@ -0,0 +1,30 @@
+package lecture02.kotlin
+
+import java.lang.IllegalArgumentException
+
+fun startsWithA1(str: String?): Boolean {
+    if (str == null) {
+        throw IllegalArgumentException("null이 들어왔습니다.")
+    }
+    return str.startsWith("A")
+}
+
+fun startsWithA2(str: String?): Boolean? {
+    if (str == null) {
+        return null
+    }
+    return str.startsWith("A")
+}
+
+fun startsWithA3(str: String?): Boolean {
+    if (str == null) {
+        return false
+    }
+    return str.startsWith("A")
+}
+
+fun main() {
+    val str: String? = null
+    println(str?.length)
+    println(str?.length ?: 0)
+}
