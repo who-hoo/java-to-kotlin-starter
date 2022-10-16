2c1a690b9def84d759d00127b4e24e014b6d9026
who-hoo <who.ho3ov@gmail.com>
2022-10-16 23:11:45 +0900

    ♻️ [9강] 부생성자가 아닌 default parameter를 이용하여 깔끔한 코드로 개선

--- src/lecture09/kotlin/Person.kt
+++ src/lecture09/kotlin/Person.kt
@@ -1,22 +1,13 @@
 package lecture09.kotlin
 
 class Person(
-    val name: String,
-    var age: Int
+    val name: String = "홍길동",
+    var age: Int = 1,
 ) {
 
     init {
         if (age <= 0) {
             throw IllegalArgumentException("나이(${age})는 1이상이어야 합니다.");
         }
-        println("초기화 블록")
-    }
-
-    constructor(name: String) : this(name, 1) {
-        println("첫 번째 부생성자")
-    }
-
-    constructor() : this("홍길동") {
-        println("두 번째 부생성자")
     }
 }
