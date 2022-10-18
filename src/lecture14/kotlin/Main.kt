ec5a96128701770a0e549ff1e0cc5c82a8d84472
who-hoo <who.ho3ov@gmail.com>
2022-10-19 00:48:42 +0900

    ✨ [14강] 코틀린에서 다양한 클래스를 다루는 방법 - Enum Class

---
+++ src/lecture14/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package lecture14.kotlin
+
+fun main() {
+    handleCountry(Country.KOREA)
+}
+
+fun handleCountry(country: Country) {
+    when (country) {
+        Country.KOREA -> TODO()
+        Country.AMERICA -> TODO()
+    }
+}
