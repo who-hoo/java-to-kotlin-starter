2e6e3067c749575cd036b1072a51bd4a41921c73
who-hoo <who.ho3ov@gmail.com>
2022-10-23 23:26:55 +0900

    ✨ [15강] 코틀린에서 배열과 컬렉션을 다루는 방법 - Collection(List)

--- src/lecture15/java/Main.java
+++ src/lecture15/java/Main.java
@@ -1,5 +1,8 @@
 package lecture15.java;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +10,18 @@ public class Main {
 
         for (int i = 0; i < array.length; i++) {
             System.out.printf("%s %s%n", i, array[i]);
+        }
+
+        final List<Integer> numbers = Arrays.asList(100, 200);
+        // 하나를 가져오기
+        System.out.println(numbers.get(0));
+        // for each
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
+        // 전통적인 for문
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.printf("%s %s%n", i, numbers.get(i));
         }
     }
 }
--- src/lecture15/kotlin/Main.kt
+++ src/lecture15/kotlin/Main.kt
@@ -11,5 +11,27 @@ fun main() {
     for ((idx, value) in array.withIndex()) { // 인덱스와 값을 한 번에 가져올 수 있다.
         println("$idx $value")
     }
+
+    val numbers = listOf(100, 200) // 불변 리스트 생성
+    val emptyList = emptyList<Int>() // 빈 리스트를 만들 때는 타입 생략 불가
+    printNumbers(emptyList()) // 타입 추론이 가능할 때는 타입 생략
+    // 하나를 가져오기
+    println(numbers[0])
+    // for each
+    for (number in numbers) {
+        println(number)
+    }
+    // 전통적인 for문
+    for ((idx, number) in numbers.withIndex()) {
+        println("$idx $number")
+    }
+
+    val mutableNumbers = mutableListOf(100, 200) // 가변 리스트 생성
+    mutableNumbers.add(300)
 }
 
+private fun printNumbers(numbers: List<Int>) {
+    for (number in numbers) {
+        println(number)
+    }
+}
