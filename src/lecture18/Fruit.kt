2c3e0372c64e7adcd34ee340140d9c415df5c871
who-hoo <who.ho3ov@gmail.com>
2022-10-24 01:48:48 +0900

    ✨ [18강] 코틀린에서 컬렉션을 함수형으로 다루는 방법

---
+++ src/lecture18/Fruit.kt
@@ -0,0 +1,11 @@
+package lecture18
+
+data class Fruit(
+    val id: Long,
+    val name: String,
+    val factoryPrice: Long,
+    val currentPrice: Long
+) {
+    val isSamePrice: Boolean
+        get() = factoryPrice == currentPrice
+}
