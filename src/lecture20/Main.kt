b2be0c5869ac181a66ef373c8db34686873ec571
who-hoo <who.ho3ov@gmail.com>
2022-10-31 00:15:41 +0900

    ✨ [20강] 코틀린의 scope function - scope function의 분류

--- src/lecture20/Main.kt
+++ src/lecture20/Main.kt
@@ -18,3 +18,19 @@ fun printPersonV2(person: Person?) {
         println(person.age)
     }
 }
+
+fun main() {
+    val person = Person("홍길동", 100)
+    val value1 = person.let { it.age }
+    println(value1)
+    val value2 = person.run { this.age }
+    println(value2)
+    val value3 = person.also { it.age }
+    println(value3)
+    val value4 = person.apply { this.age }
+    println(value4)
+    with(person) {
+        println(name)
+        println(age)
+    }
+}
