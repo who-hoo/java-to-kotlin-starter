b4e48b42f638cb5df6c5c87ec7e6b73a54b37211
who-hoo <who.ho3ov@gmail.com>
2022-10-31 02:04:30 +0900

    ✨ [20강] 코틀린의 scope function - 언제 어떤 scope function을 사용해야 할까

--- src/lecture20/Main.kt
+++ src/lecture20/Main.kt
@@ -5,6 +5,11 @@ class Person(
     var age: Int
 )
 
+class PersonDto(
+    val name: String,
+    var age: Int
+)
+
 fun printPersonV1(person: Person?) {
     if (person != null) {
         println(person.name)
@@ -32,5 +37,56 @@ fun main() {
     with(person) {
         println(name)
         println(age)
+    }
+
+    //let
+    // 하나 이상의 함수를 call chain 결과로 호출할 때
+    val strings = listOf("APPLE", "CAR")
+    strings.map { string -> string.length }
+        .filter { length -> length > 3 }
+        .let(::println)
+    // non-null 값에 대해서만 code block을 실행시킬 때
+    val strs: List<String?> = listOf("apple", null)
+    val length = strs?.let { str ->
+        println(str.upperCase())
+        str.length
+    }
+    // 일회성으로 제한된 영역에 지역 변수를 만들 때
+    val numbers = listOf("one", "two", "three")
+    val modifiedFirstItem = numbers.first()
+        .let { firstItem ->
+            if (firstItem.length >= 5) firstItem else "!$firstItem!"
+        }.uppercase()
+    println(modifiedFirstItem)
+
+    //run
+    // 객체 초기화와 반환 값의 계산을 동시에 해야할 때
+    // val person = Person("홍길동", 100).run(personRepository::save)
+
+    //apply
+    // 객체 설정을 할 때에 객체를 수정하는 로직이 call chain 중간에 필요할 때(ex.test fixture)
+    fun createPerson(
+        name: String,
+        age: Int
+    ): Person {
+        return Person(
+            name = name,
+            age = 0
+        ).apply { this.age = age }
+    }
+
+    //also
+    // 객체를 수정하는 로직이 call chain 중간에 필요할 때
+    mutableListOf("one", "two", "three")
+        .also { println("The list elements before adding new one: $it") }
+        .add("four")
+
+    //with
+    // 특정 객체를 다른 객체로 변환해야 하는데,
+    // 모듈 간의 의존성에 의해 정적 팩토리 혹은 toClass 함수를 만들기 어려울 때
+    fun withExample(person: Person) {
+        return with(person) {
+            PersonDto(name = name, age = age)
+        }
     }
 }
